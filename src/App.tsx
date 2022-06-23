5343d0e7f0eb0ebac4430ef2be8954d4200f5202
Aliaksei <aliaksei.pahirnitski.edu@gmail.com>
2022-06-23 17:14:14 +0300

    Adding formatting on saving

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,20 @@ import React from 'react';
 import './App.css';
 import QuestionCard from './screens/QuestionScreen';
 
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Starting with boilerplate</h1>
         <QuestionCard></QuestionCard>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <p>Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="App-link" href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+          Learn React F        </a>      </header>
     </div>
   );
 }
--- src/screens/QuestionScreen.tsx
+++ src/screens/QuestionScreen.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 
-function QuestionCard(){
+function QuestionCard() {
     let a = 5;
     const b = 3;
     let c = a - b;
-console.log(c);
+    console.log(c);
+    let veryLongNameWithLendth25 = 5;
+    veryLongNameWithLendth25 = veryLongNameWithLendth25
+        + veryLongNameWithLendth25 + veryLongNameWithLendth25 + veryLongNameWithLendth25 + veryLongNameWithLendth25 + veryLongNameWithLendth25 + veryLongNameWithLendth25
+        + 8;
+
+
+
+    c = veryLongNameWithLendth25;
     c = a + b;
     return (
-        <div > IT IS QUESTION HERE: 
+        <div > IT IS QUESTION HERE:
             <span> {a} + {b}</span>=<span>{c}</span></div>
     );
 
-
 }
+
 
 export default QuestionCard;
